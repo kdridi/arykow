05673fcdec78a03ae8f5a4cb02dcf8419c14db35
Karim DRIDI <seth.holth@gmail.com>
2010-10-23 20:49:01 +0200

    Initialize nokogiri sample for leboncoin.fr website

--- www/hello_world.rb
+++ www/hello_world.rb
@@ -2,10 +2,28 @@ require 'rubygems'
 
 # If you're using bundler, you will need to add these 2 lines
 require 'bundler'
+require 'haml'
 Bundler.setup
 
 require 'sinatra'
 
 get '/' do
   haml :index
+end
+
+get '/leboncoin/:query' do |query|
+  require 'open-uri'
+  require 'nokogiri'
+  require 'htmlentities'
+  @result = Hash.new
+  @result['query'] = query
+  @result['entries'] = Array.new
+  coder = HTMLEntities.new
+  Nokogiri::HTML(open('http://www.leboncoin.fr/annonces/offres/?f=a&th=1&q=' + query)).xpath('//table[@id="hl"]/tr/td[3]/a').each do |node|
+    entry = Hash.new
+    entry['title'] = coder.encode node.content.strip
+    entry['link'] = node['href']
+    @result['entries'].push entry
+  end
+  haml :leboncoin
 end
